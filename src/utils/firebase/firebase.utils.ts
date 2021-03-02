7fb5434c722d4e2bbdd74aa09e623387a7e7a641
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-03-03 01:04:45 +0900

    Update firebase auth setup

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -16,3 +16,12 @@ firebase.initializeApp( config );
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters( {
+  prompt:'select_account'
+} );
+
+export const signInWithGoogle = () =>auth.signInWithPopup( provider );
+
+export default firebase;
