310c488c1ee666911531ed18a54a7953c94b0785
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-03-05 01:02:54 +0900

    Create cart-item component and connected cart dropdown with cart item

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import "./cart-dropdown.styles.scss";
 
 import CustomButton from "components/custom-button/custom-button.component";
+import CartItem from "components/cart-item/cart-item.component";
+import { useCart } from "utils/redux/cart/cart.hooks";
 
 const CartDropDown = () => {
+  const { cartItems } = useCart();
+  
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"/>      
+      <div className="cart-items">
+        {cartItems.map( cartItem => <CartItem key={cartItem.id} item={cartItem}/> )}
+      </div>      
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
---
+++ src/components/cart-item/cart-item.component.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CartItem as CartItemType } from "utils/redux/cart/cart.reducer";
+import "./cart-item.styles.scss";
+
+
+type CartItemProps = {
+  item :CartItemType
+}
+
+const CartItem = ( { item: { imageUrl, price, name, quantity } }:CartItemProps ) => {
+  
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt={name}/>
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">{quantity} x ${price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
