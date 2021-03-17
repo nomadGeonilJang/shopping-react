f2e817650427344872958dad0faf5e5f39b81e81
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-03-17 23:14:01 +0900

    seperatin clent and server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const cors = require("cors");
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production'){
@@ -11,21 +11,18 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', function(req, res) {
+  app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.listen(port, error => {
-  if (error) throw error;
-  console.log('Server running on port ' + port);
-});
 
 app.post('/payment', (req, res) => {
   const body = {
@@ -41,4 +38,11 @@ app.post('/payment', (req, res) => {
       res.status(200).send({ success: stripeRes });
     }
   });
+
+});
+
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
 });
